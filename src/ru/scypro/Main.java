d04873cb8fd6aadc2295aba8ff4fd5a93d36dd10
Александр Бордюжа <a.n.brdz@gmail.com>
2022-04-04 19:26:56 +0300

    второй комит

--- src/ru/scypro/Main.java
+++ src/ru/scypro/Main.java
@@ -3,7 +3,18 @@ package ru.scypro;
 public class Main {
 
     public static void main(String[] args) {
-	// Задача 1
+
+        // Задача 1
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i++);
+        }
+        System.out.print("\n");
+        for (int a = 10; a >= 0; a--) {
+            System.out.print(a);
+        }
+        // Задача 2
 
 
     }
