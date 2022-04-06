337e77d54a482ace0241c6063aff807def8aba52
Александр Бордюжа <a.n.brdz@gmail.com>
2022-04-06 11:48:53 +0300

    третий комит

--- src/ru/scypro/Main.java
+++ src/ru/scypro/Main.java
@@ -13,9 +13,26 @@ public class Main {
         System.out.print("\n");
         for (int a = 10; a >= 0; a--) {
             System.out.print(a);
+
         }
+
         // Задача 2
 
+        System.out.print("\n");
+        for (int firstFriday = 1; firstFriday < 31; firstFriday = firstFriday + 7)  {
+            System.out.println("Сегодня пятница," + firstFriday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        // Задача 3
+
+        for (int year = 0; year < 2100; year = year + 79) {
+            if (year > 1817)  {
+                System.out.println(year);
+            }
+        }
+
+
+        }
 
     }
-}
+
